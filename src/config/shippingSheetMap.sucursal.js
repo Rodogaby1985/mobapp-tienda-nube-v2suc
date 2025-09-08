e6ecca893284f2024d88e772856baace64aaeb63
Rodogaby1985 <rodogaby85@gmail.com>
2025-09-07 23:26:06 -0300

    Delete src/config directory

--- src/config/shippingSheetMap.sucursal.js
+++
@@ -1,9 +0,0 @@
-// Mapeo exclusivo para modalidad SUCURSAL.
-// Si agregas nuevas transportistas sucursal, sólo sumas aquí.
-module.exports = {
-  "ANDREANI A SUCURSAL": "ANDREANI SUC",
-  "CORREO ARGENTINO A SUCURSAL": "CA SUC",
-  "OCA A SUCURSAL": "OCA SUC",
-  // Ejemplo futuro:
-  // "LOGEX A SUCURSAL": "LOGEX SUC",
-};
