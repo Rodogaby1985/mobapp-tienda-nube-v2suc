bbd5484969f09ae16ad0d818513a426d22e4718d
Rodogaby1985 <rodogaby85@gmail.com>
2025-09-07 23:19:06 -0300

    Add shippingSheetMap for SUCURSAL transportistas

---
+++ src/config/shippingSheetMap.sucursal.js
@@ -0,0 +1,9 @@
+// Mapeo exclusivo para modalidad SUCURSAL.
+// Si agregas nuevas transportistas sucursal, sólo sumas aquí.
+module.exports = {
+  "ANDREANI A SUCURSAL": "ANDREANI SUC",
+  "CORREO ARGENTINO A SUCURSAL": "CA SUC",
+  "OCA A SUCURSAL": "OCA SUC",
+  // Ejemplo futuro:
+  // "LOGEX A SUCURSAL": "LOGEX SUC",
+};
